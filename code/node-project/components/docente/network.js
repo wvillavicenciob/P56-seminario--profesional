01a8fbb97a441417c66f00a4cd35949b1438615b
wvillavicenciob <71676005+wvillavicenciob@users.noreply.github.com>
2020-10-01 18:30:57 -0500

    node.js

--- code/node-project/components/docente/network.js
+++ code/node-project/components/docente/network.js
@@ -1,17 +1,46 @@
 const express = require('express')
 const response = require('../../network/response')
+const controller = require('./controller')
 
 const router = express.Router()
 
 router.get('/', function(req, res) {
-    response.success( req, res, 'Lista de Docentes de la UPS.', 200 )
-})
-router.post('/', function(req, res) {
-    if (req.query.error == 'ok') {
-        response.error( req, res, 'Error al ingresar Docente.', 500 )        
-    } else {
-        response.success( req, res, 'Ingreso de Docente exitoso.', 201 )        
-    }
+    const filtroDocente = req.query.docente || null
+    controller.getDocentes( filtroDocente )
+        .then((data) => {
+            response.success( req, res, data, 200 )
+        })
+        .catch((error) => {
+            response.error( req, res, error, 500 )
+        })
 })
 
-module.exports = router
+router.post('/', function(req, res) {
+    controller.addDocente( req.body.nombre, req.body.apellido, req.body.correoElectronico )
+        .then((data) => {
+            response.success( req, res, data, 201 )        
+        })
+        .catch((error) => {
+            response.error( req, res, error, 500 )        
+        })
+})
+
+router.patch('/', function(req, res) {
+    controller.updateDocente( req.body.id_docente, req.body.nombre, req.body.apellido, req.body.correoElectronico )
+        .then((data) => {
+            response.success( req, res, data, 201 )        
+        })
+        .catch((error) => {
+            response.error( req, res, error, 500 )        
+        })
+})
+
+router.delete('/', function(req, res) {
+    controller.deleteDocente( req.body.id_docente )
+        .then((data) => {
+            response.success( req, res, data, 201 )        
+        })
+        .catch((error) => {
+            response.error( req, res, error, 500 )        
+        })
+})
